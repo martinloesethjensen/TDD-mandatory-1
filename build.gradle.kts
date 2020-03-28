2ae5ee1ac3d0327f1b68b79fef36c2c1770a36df
Martin L. Jensen <martinloesethjensen@gmail.com>
2020-03-28 11:40:29 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+val junitVersion: String by project
+val mockkVersion: String by project
+val kluentVersion: String by project
+
+plugins {
+    kotlin("jvm") version "1.3.70"
+    java
+    idea
+}
+
+group = "dk.martin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = junitVersion)
+    testImplementation("org.amshove.kluent:kluent:$kluentVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "tdd-mandatory-1"
+
